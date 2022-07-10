e0ea74244e2d04ca3bee5c0b9ab156c9b1c8700d
Cintra <64995266+CintraB@users.noreply.github.com>
2022-07-09 21:00:57 -0300

    gráfico 1
    agr da p fazer pesquisa por IP no gráfico 1

--- php/snmpIP.php
+++ php/snmpIP.php
@@ -1,11 +1,16 @@
 
 <?php
-	$destIP = "localhost";
-	$community = "gerencia";
-	$oid = ".1.3.6.1.2.1.4.9.0";
+#$destIP = "192.168.1.11";
+#$destIP = addslashes($_POST['campoIP']);
+#$destIP = "localhost";
+$destIP = $_POST['campoIP'];
+$community = "gerencia";
+$oid = ".1.3.6.1.2.1.4.9.0";
 
-	$ipInDelivers = snmp2_get("$destIP","$community","$oid");
-	
-	$ipIn = explode(" ",$ipInDelivers);
-	echo $ipIn[1];
+$ipInDelivers = snmp2_get("$destIP", "$community", "$oid");
+
+$ipIn = explode(" ", $ipInDelivers);
+echo $ipIn[1];
+
+
 ?>
--- php/snmpout.php
+++ php/snmpout.php
@@ -1,5 +1,7 @@
 <?php
-	$destIP = "localhost";
+	$destIP = $_POST['campoIP'];
+	#$destIP = "localhost";
+	#$destIP = addslashes($_POST['campoIP']);
 	$community = "gerencia";
 	$oid = ".1.3.6.1.2.1.4.10.0";
 
